843632011b2b2d1f672be8b7cbc86677fc0d106a
ShizzaHo <74906456+ShizzaHo@users.noreply.github.com>
2022-04-02 11:49:40 +0500

    Add files via upload

--- scripts/projects.js
+++ scripts/projects.js
@@ -27,12 +27,18 @@ class Project {
 /* */
 
 /* */
-tools.push("TelegramWeb ChatMoveToRight","Расширение для chromium браузеров которое переместит ваш чат TelegramWeb влево","07.02.2022","Extensions, JS, HTML, CSS","Открыть репозиторий в GitHub","ChatMoveToRight","https://i.ibb.co/2Wt7csG/image.png");
+tools.push(new Project("TelegramWeb ChatMoveToLeft","Расширение для chromium браузеров которое переместит ваш чат TelegramWeb влево","07.02.2022","Extensions, JS, HTML, CSS","Открыть репозиторий в GitHub","ChatMoveToRight","https://sun9-87.userapi.com/c637725/v637725736/1c77b/Tp0EeYMJwiU.jpg"));
+tools.push(new Project("SZH Remote (Проект заморожен)","Приложение для удаленного управления Windows, управление работает на любом устройстве на котором есть веб-браузер","15.02.2022","JS, HTML, CSS, NodeJS, ElectronJS, ExpressJS","Открыть репозиторий в GitHub","SZHRemote","https://i.ibb.co/y0P7R0V/image.png"));
 /* */
 
 /* */
 function ChatMoveToRight(){
-    
+    window.open("https://github.com/ShizzaHo/TelegramWeb-ChatMoveToLeft");
+}
+
+function SZHRemote(){
+    window.open("https://github.com/ShizzaHo/SZH-Remote");
 }
 /* */
+
 
